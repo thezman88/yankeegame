6f0889aec5e5ceeb4dd2db48a38af938c3de264a
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-08-19 18:54:49 -0400

    global variables added

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+// global variables
+var win = 0;
+var loss = 0;
+var answer = ['ruth','dimaggio','mantel','jeter','rivera'];
+var randNum = Math.floor(Math.random()*answer.length);
+var currentWord = answer[randNum];
