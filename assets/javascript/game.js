947ce8d224d81d5ea5c0925d2d6a669eea884ecf
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-08-23 16:40:07 -0400

    it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,7 +24,7 @@ function newGame() {
   theWord = answer[Math.floor(Math.random() * answer.length)];
   console.log(theWord);
   // grab theWord and split it up to each letter
-  theWordLtrs = currentWord.split("");
+  theWordLtrs = theWord.split("");
   console.log(theWordLtrs);
   // get number of letters from the word
   underScores = theWordLtrs.length;
@@ -58,5 +58,137 @@ function newGame() {
   document.getElementById('wins').innerHTML = "Wins:" + " " + wins;
   document.getElementById('losses').innerHTML = "Losses:" + "" + losses;
 
-
 }
+function checkLtrs(letter) {
+
+  //Check if the letter pressed is an actual letter
+  if (event.keyCode >= 65 && event.keyCode <= 90) { //If the letter pressed IS part of the alphabet, then run the comparison:
+
+        //Check if the letter guessed is one of the letters in the word
+        var correctLetter = false;
+
+        for ( var i = 0; i < underScores; i++) {
+          if(theWord[i] == letter) {
+            correctLetter = true;
+          }
+        }
+
+        //Check where the correct letter is located on the word, then add to html
+        if(correctLetter) {
+          for ( var i = 0; i < underScores; i++) {
+            if(theWord[i] == letter) {
+              answerDisplay[i] = letter;
+            }
+          }
+        }
+
+        //If the letter isn't part of the word
+        else {
+          wrongLtrs.push(letter);
+        guessesRemain--
+        }
+
+        //testing via console
+        console.log(answerDisplay);
+
+  } else { //If user input is not a letter from the alphabet
+    // Alert the user
+    alert("Please be sure to select a letter from the Alphabet (from a to z)");
+  }
+}
+function roundComplete() {
+		console.log("Win count: " + wins +  " | Loss Count: " + losses + " | Guesses Left: " + guessesRemain)
+
+		//Update HTML with Game Stats
+		document.getElementById("remainingGuesses").innerHTML = "Guesses Remaining: " + " " + guessesRemain;
+		document.getElementById("currentWord").innerHTML = answerDisplay.join(" ");
+		document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed:" + " " + wrongLtrs.join(" ");
+
+
+		//Check if the user won
+		if (theWordLtrs.toString() == answerDisplay.toString()) {
+			wins++;
+			alert("Theeeee Yankees Win! You got '" + theWord + "' correctly. Try another round?");
+			console.log("YOU WIN!");
+
+			// Update the wins in the HTML doc
+			document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
+
+			//Start New Game and clear letters already guessed
+			newGame();
+			document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed:" + " " + " ";
+
+		} else if (guessesRemain == 0) { //Check if user lost
+			losses++;
+			alert("You have 0 guesses left,Your Out!. The word was '" + theWord + "'. Do you want to try again?")
+			console.log("You Lost!");
+
+			// Update the wins in the HTML doc
+			document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
+
+			//Start New Game
+			newGame();
+			document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed:" + " " + " ";
+
+		}
+	}
+
+
+  // MAIN PROCESS
+//=============
+
+	//Call function to start the game for the first time
+	newGame();
+
+	//Get input from user on what keys are being pressed
+	document.onkeyup = function(event) {
+		//Create a variable to hold all the letters that have been guessed
+		var ltrsGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+			console.log("You Guessed the letter: " + ltrsGuessed); // Testing via Console.Log
+
+		//Run the check letters function
+		checkLtrs(ltrsGuessed);
+		roundComplete();
+
+		//Game interation with the images (Guesses Left, as displayed by images)
+		if (guessesRemain <= 8) {
+			document.getElementById("homerunImg").style.transform = "rotate(180deg)";
+			document.getElementById("homerunImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 7) {
+			document.getElementById("playerImg").style.transform = "rotate(180deg)";
+			document.getElementById("playerImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 6) {
+			document.getElementById("hotDogImg").style.transform = "rotate(180deg)";
+			document.getElementById("hotDogImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 5) {
+			document.getElementById("batGloveImg").style.transform = "rotate(180deg)";
+			document.getElementById("batGloveImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 4) {
+			document.getElementById("gloveImg").style.transform = "rotate(180deg)";
+			document.getElementById("gloveImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 3) {
+			document.getElementById("stadiumImg").style.transform = "rotate(180deg)";
+			document.getElementById("stadiumImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 2) {
+			document.getElementById("baseballImg").style.transform = "rotate(180deg)";
+			document.getElementById("baseballImg").style.opacity = "0.25";
+		}
+
+		if (guessesRemain <= 1) {
+			document.getElementById("batBallImg").style.transform = "rotate(180deg)";
+			document.getElementById("batBallImg").style.opacity = "0.25";
+		}
+
+	}
