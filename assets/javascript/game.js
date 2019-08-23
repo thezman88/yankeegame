dea6fb64b2d9d9206d58ef42d1157fd83101091d
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-08-22 23:05:30 -0400

    new javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// answer
+var answer = ['ruth','mantel','dimaggio','jeter','rivera'];
+// the word guessed as string
+var theWord = "";
+// array to hold letters in theWord
+var theWordLtrs = [];
+// number of _ in theWord
+var underScores = 0;
+// array to store displayed answer
+var answerDisplay = [];
+// array for wrong guesses displayed
+var wrongLtrs =[];
